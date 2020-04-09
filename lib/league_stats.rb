adf70074855cba43d768fe315324d8dd08de85ca
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 20:48:40 -0600

    Pass test_it_can_return_team__name_with_highest_average_goals

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -6,16 +6,55 @@ class LeagueStats
     @teams = teams
   end
 
-  def goals_by_team
-    games_by_team = @game_teams.group_by do |game|
-      game.team_id
-    end
-    games_by_team.transform_values do |group|
-      group.map {|game| game.goals}
-    end
+  # def goals_by_team(team_id)
+  #   @game_teams.sum {|goals| }
+  # end
+
+  def find_by_collection(team_id, csv_header, game_teams)
+    @game_teams.find_all { |team| team.send(csv_header) == team_id }
   end
 
-  def average_num_of_goals
-    @game_teams
+  def games_by_team(team_id)
+    find_by_collection(team_id, "team_id", @game_teams)
   end
+
+  def total_games_by_team_id(team_id)
+      games_by_team(team_id).length
+  end
+
+  def total_goals_by_team_id(team_id)
+    games_by_team(team_id).sum { |game_team| game_team.goals }
+  end
+
+  def average_goals_per_team(team_id)
+    (total_goals_by_team_id(team_id).to_f / total_games_by_team_id(team_id).to_f).round(2)
+  end
+
+  def highest_average_goals
+  game_team = @game_teams.max_by {|team| average_goals_per_team(team.team_id)}
+  game_team_id = game_team.team_id
+  team = @teams.find do |team|
+    team.team_id == game_team_id
+  end
+  team.team_name
+  end
+
+
+
+  # def goals_by_team
+  #   games_by_team = @game_teams.group_by do |game|
+  #     game.team_id
+  #   end
+  #   games_by_team.transform_values do |group|
+  #     group.map {|game| game.goals}
+  #   end
+  # end
+  #
+  # def average_num_of_goals
+  #   require "pry"; binding.pry
+  #   goals = 0
+  #   goals_by_team.sum {|goal| goals += 1 }
+  #
+  #   goals / goals_by_team
+  # end
 end
--- test/league_stats_test.rb
+++ test/league_stats_test.rb
@@ -21,12 +21,37 @@ class LeagueStatsTest < Minitest::Test
     assert_equal @teams, @league_stats.teams
   end
 
-  def test_goals_by_team
-    assert_equal [3, 3, 2, 3, 3, 3, 4, 2, 1], @league_stats.goals_by_team[6]
+  # def test_goals_by_team
+  #
+  #   assert_equal [3, 3, 2, 3, 3, 3, 4, 2, 1], @league_stats.goals_by_team(6)
+  # end
+
+  def test_can_find_by_collection
+    assert_instance_of Array, @league_stats.find_by_collection(6, "team_id", @game_teams)
+    assert_equal 9, @league_stats.find_by_collection(6, "team_id", @game_teams).count
   end
 
   def test_average_number_of_goals
-    skip
+skip
     assert_equal 3.5, @league_stats.average_num_of_goals
   end
+
+  def test_it_can_return_games_by_team
+    assert_equal 9, @league_stats.games_by_team(6).count
+  end
+# total_games_by_team_id(team_id)
+  def test_it_can_return_average_goals_per_team
+    assert_equal 2.67, @league_stats.average_goals_per_team(6)
+  end
+
+  def test_it_can_return_team__name_with_highest_average_goals
+
+    assert_equal "FC Dallas", @league_stats.highest_average_goals
+  end
+
+  def test_it_can_return_name_of_team_with_highest_average_goals
+    skip
+    assert_equal "Team Name", @league_stats.highest_average_goals_team_name
+  end
+
 end
