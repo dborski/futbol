9088a832a64d6803e2534271145ea53a10760b64
Derek Borski <derek.borski@gmail.com>
2020-04-08 23:22:02 -0600

    Creates helper classes game_ids_by_season and games_by_season_id along with tests

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,8 +1,30 @@
 class SeasonStats
 
-  attr_reader :game_teams, :teams
-  def initialize(game_teams, teams)
+  attr_reader :game_teams, :games, :teams
+  def initialize(game_teams, games, teams)
     @game_teams = game_teams
+    @games = games
     @teams = teams
+  end
+
+  def games_ids_by_season(season_id)
+    all_games_by_id = @games.find_all {|game| game.season == season_id}
+    all_games_by_id.map { |game| game.game_id}
+  end
+
+  def games_by_season_id(season_id)
+    games_by_id = []
+    @game_teams.each do |game|
+      if games_ids_by_season(season_id).any? { |id| id == game.game_id}
+        games_by_id << game
+      end
+    end
+    games_by_id
+  end
+
+  def winningest_coach(season_id)
+    #finding all games by season id
+    require "pry"; binding.pry
+    #find winningest coach by that season
   end
 end
--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -7,9 +7,10 @@ require './test/test_helper'
 class SeasonStatsTest < Minitest::Test
 
   def setup
-    @game_teams = GameTeam.from_csv("./test/fixtures/game_teams_truncated.csv")
+    @game_teams = GameTeam.from_csv("./test/fixtures/game_teams_truncated_new.csv")
+    @games = Game.from_csv("./test/fixtures/game_truncated_new.csv")
     @teams = Team.from_csv("./data/teams.csv")
-    @season_stats = SeasonStats.new(@game_teams, @teams)
+    @season_stats = SeasonStats.new(@game_teams, @games, @teams)
   end
 
   def test_it_exists
@@ -18,6 +19,45 @@ class SeasonStatsTest < Minitest::Test
 
   def test_it_has_readable_attributes
     assert_equal @game_teams, @season_stats.game_teams
+    assert_equal @games, @season_stats.games
     assert_equal @teams, @season_stats.teams
   end
+
+  def test_all_games_by_season
+    assert_equal 30, @season_stats.games_ids_by_season("20122013").length
+    assert_equal 30, @season_stats.games_ids_by_season("20132014").length
+    assert_equal 30, @season_stats.games_ids_by_season("20142015").length
+    assert_equal 30, @season_stats.games_ids_by_season("20162017").length
+    assert_equal 30, @season_stats.games_ids_by_season("20172018").length
+  end
+
+  def test_games_by_season_id
+    assert_equal 60, @season_stats.games_by_season_id("20122013").length
+    assert_equal 60, @season_stats.games_by_season_id("20132014").length
+    assert_equal 60, @season_stats.games_by_season_id("20142015").length
+    assert_equal 60, @season_stats.games_by_season_id("20162017").length
+    assert_equal 60, @season_stats.games_by_season_id("20172018").length
+  end
+
+  def test_winningnest_coach
+    skip
+    assert_equal "Mike Babcok", @season_stats.winningest_coach("20122013")
+    assert_equal "Mike Babcok", @season_stats.winningest_coach("20132014")
+  end
 end
+
+# Season Statistics
+# These methods each take a season id as an argument and return the values described below.
+#
+# Method	Description	Return Value
+# winningest_coach	Name of the Coach with the best win percentage for the season	String
+
+# worst_coach	Name of the Coach with the worst win percentage for the season	String
+
+# most_accurate_team	Name of the Team with the best ratio of shots to goals for the season	String
+
+# least_accurate_team	Name of the Team with the worst ratio of shots to goals for the season	String
+
+# most_tackles	Name of the Team with the most tackles in the season	String
+
+# fewest_tackles	Name of the Team with the fewest tackles in the season	String
