2ce792fe766152d3a2af40664b1b86a70f56631d
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-09 16:43:26 -0600

    change test to use new fixture files

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -7,9 +7,11 @@ require './test/test_helper'
 class TeamStatsTest < Minitest::Test
 
   def setup
-    @game_teams = GameTeam.from_csv("./test/fixtures/game_teams_truncated.csv")
+    @game_teams = GameTeam.from_csv("./test/fixtures/game_teams_truncated_new.csv")
     @teams = Team.from_csv("./data/teams.csv")
-    @team_stats = TeamStats.new(@game_teams, @teams)
+    @games = Game.from_csv("./test/fixtures/game_truncated_new.csv")
+    @team_stats = TeamStats.new(@game_teams, @teams, @games)
+
   end
 
   def test_it_exists
@@ -19,5 +21,46 @@ class TeamStatsTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal @game_teams, @team_stats.game_teams
     assert_equal @teams, @team_stats.teams
+    assert_equal @games, @team_stats.games
+  end
+
+  def test_it_can_get_team_info
+    expected = {team_id: 1, franchise_id: 23,
+                team_name: "Atlanta United", abbreviation: "ATL",
+                link: "/api/v1/teams/1"
+                }
+    assert_equal expected, @team_stats.team_info(1)
+  end
+
+  def test_it_can_get_season_from_game_id
+    assert_equal "20122013", @team_stats.season_from_game(2012030221)
+  end
+
+  def test_it_can_get_games_played_in
+    assert_equal 25, @team_stats.games_played_in(6).length
+    assert_equal 25, @team_stats.games_played_in(17).length
+
+    @team_stats.games_played_in(6).each do |game|
+      assert_instance_of GameTeam, game
+    end
+
+    @team_stats.games_played_in(6).each do |game|
+      assert_equal 6, game.team_id
+    end
+  end
+
+  def test_it_can_get_average_win_percentage
+    assert_equal 57.14, @team_stats.average_win_percentage(17)
+    assert_equal 100.00, @team_stats.average_win_percentage(6)
+  end
+
+  def test_it_can_get_most_goals_scored
+    assert_equal 3, @team_stats.most_goals_scored(17)
+    assert_equal 4, @team_stats.most_goals_scored(5)
+  end
+  def test_it_can_get_fewest_goals_scored
+    assert_equal 0, @team_stats.fewest_goals_scored(17)
+    assert_equal 0, @team_stats.fewest_goals_scored(6)
+    assert_equal 0, @team_stats.fewest_goals_scored(5)
   end
 end
