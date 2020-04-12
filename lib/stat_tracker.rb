a08f2928ed4e0b509c01c01bcdf5c06e21f718d6
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-12 09:23:07 -0600

    Add game methods and tests to stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,6 @@
 class StatTracker
-  attr_reader :game_path, :teams_path, :game_teams_path
+  attr_reader :games, :teams, :game_teams, :league_stats, :sea
+
   def self.from_csv(file_paths)
     game_path = file_paths[:games]
     teams_path = file_paths[:teams]
@@ -8,22 +9,49 @@ class StatTracker
   end
 
   def initialize(game_path, teams_path, game_teams_path)
-    @game_path = game_path
-    @teams_path = teams_path
-    @game_teams_path = game_teams_path
-  end
-
-  def games
-    Game.from_csv(@game_path)
-    Game.all
-  end
-
-  def teams
-    Team.from_csv(@game_path)
-    Team.all
+    @games = Game.from_csv(game_path)
+    @teams = Team.from_csv(teams_path)
+    @game_teams = GameTeam.from_csv(game_teams_path)
+    @league_stats = LeagueStats.new(@game_teams, @teams)
+    @season_stats = SeasonStats.new(@game_teams, @games, @teams)
+    # @team_stats = TeamStats(@game_teams, @games, @teams)
   end
 
   def count_of_teams
     Team.all.length
   end
+
+  def highest_total_score
+    Game.highest_total_score
+  end
+
+  def lowest_total_score
+    Game.lowest_total_score
+  end
+
+  def percentage_home_wins
+    Game.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    Game.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    Game.percentage_ties
+  end
+
+  def count_of_games_by_season
+    Game.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    Game.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    Game.average_goals_by_season
+  end
+
+  
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -2,16 +2,20 @@ require './test/test_helper'
 require './lib/stat_tracker'
 require './lib/game'
 require './lib/team'
+require './lib/game_team'
+require './lib/league_stats'
+require './lib/season_stats'
+# require './lib/team_stats'
 
 class StatTrackerTest < Minitest::Test
   def setup
     game_path = './data/games.csv'
-    team_path = './data/teams.csv'
+    teams_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
 
     locations = {
       games: game_path,
-      teams: team_path,
+      teams: teams_path,
       game_teams: game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(locations)
@@ -22,9 +26,10 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_has_readable_attributes
-    assert_equal './data/games.csv', @stat_tracker.game_path
-    assert_equal './data/teams.csv', @stat_tracker.teams_path
-    assert_equal './data/game_teams.csv', @stat_tracker.game_teams_path
+    assert_instance_of Game, @stat_tracker.games[0]
+    assert_instance_of Team, @stat_tracker.teams[0]
+    assert_instance_of GameTeam, @stat_tracker.game_teams[0]
+    assert_instance_of LeagueStats, @stat_tracker.league_stats
   end
 
   def test_from_csv_creates_array_of_all_games
@@ -36,5 +41,57 @@ class StatTrackerTest < Minitest::Test
 
   def test_returns_count_of_teams
     assert_equal 32, @stat_tracker.count_of_teams
+  end
+
+  def test_it_can_return_highest_total_score
+    assert_equal 11, @stat_tracker.highest_total_score
+  end
+
+  def test_it_can_return_lowest_total_score
+    assert_equal 0, @stat_tracker.lowest_total_score
+  end
+
+  def test_it_can_return_percentage_home_wins
+    assert_equal 40.0, @stat_tracker.percentage_home_wins
+  end
+
+  def test_it_can_return_percentage_visitor_wins
+    assert_equal 40.0, @stat_tracker.percentage_visitor_wins
+  end
+
+  def test_it_can_return_percentage_ties
+    assert_equal 20.0, @stat_tracker.percentage_ties
+  end
+
+  def test_it_can_return_percentage_ties
+    assert_equal 20.0, @stat_tracker.percentage_ties
+  end
+
+  def test_it_can_return_count_of_games_by_season
+    expected_hash = {
+                  "20122013" => 806,
+                  "20162017" => 1317,
+                  "20142015" => 1319,
+                  "20152016" => 1321,
+                  "20132014" => 1323,
+                  "20172018" => 1355
+                  }
+    assert_equal expected_hash, @stat_tracker.count_of_games_by_season
+  end
+
+  def test_it_can_return_average_goals_per_game
+    assert_equal 4.22, @stat_tracker.average_goals_per_game
+  end
+
+  def test_it_can_return_average_goals_by_season
+    expected_hash = {
+                  "20122013" => 4.12,
+                  "20162017" => 4.23,
+                  "20142015" => 4.14,
+                  "20152016" => 4.16,
+                  "20132014" => 4.19,
+                  "20172018" => 4.44
+                  }
+    assert_equal expected_hash, @stat_tracker.average_goals_by_season
   end
 end
