3eadf59f66d31beadaa9d5a1992cb141d2fab6b0
Norma Lopez <norma_l304@yahoo.com>
2020-04-13 21:10:29 -0600

    Add collectable module

---
+++ lib/collectable.rb
@@ -0,0 +1,8 @@
+require 'CSV'
+
+module Collectable
+  def create_objects(file_path, object)
+    csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol )
+    @@all = csv.map{|row| object.new(row)}
+  end
+end
